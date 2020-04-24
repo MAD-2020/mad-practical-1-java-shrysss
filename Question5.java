d0d9bf62b1914445486054048f5a0fbdd4bccbfe
shrysss <64087486+shrysss@users.noreply.github.com>
2020-04-24 12:55:19 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
-
+import java.util.Arrays;
 public class Question5
 {
+  
   public static void main(String[] args)
   {
     /**
@@ -26,7 +27,48 @@ public class Question5
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
      
-    Scanner in = new Scanner(System.in);
+    Scanner integer = new Scanner(System.in);
+    Scanner ListIntegers = new Scanner(System.in);
     
+    
+    while(true)
+    {
+      System.out.println("Please enter an integer");
+      int NumberOfIntegers = integer.nextInt();
+      System.out.println("now please enter "+ NumberOfIntegers + "Integers");
+      int[] list = new int[NumberOfIntegers];
+      while (NumberOfIntegers>0)
+      {
+        int NewInt = ListIntegers.nextInt();
+        list[NumberOfIntegers-1] = NewInt;
+        NumberOfIntegers -= 1;
+        
+      }
+
+      int element = 0;
+      int count = 0;
+
+      for (int j = 0 ; j< list.length ; j++){
+        int tempElement = list[j];
+        int tempCount = 0;
+        for(int p = 0; p<list.length; p++){
+          if(list[p] == tempElement){
+            tempCount++;
+          }
+          if(tempCount>count){
+            element = tempElement;
+            count= tempCount;
+          }
+        }
+        
+      }
+      
+      System.out.println("The mode is " + element);
+
+      
+    }
+    
+
+
   }
 }
